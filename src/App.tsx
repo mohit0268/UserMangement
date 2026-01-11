8656097ec31e25658370af5fb7b71b392a6d8c58
Mohit Purohit <mohitpurohit020@gmail.com>
2026-01-11 13:09:33 +0530

    Feat: Added Routes (signin,signup,dashboard)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import SignIn from "./pages/Signin";
+import SignUp from './pages/Signup';
+import Dashboard from './pages/dashboard'
+import "./App.css";
+
+
 
 function App() {
   return (
     <>
-      <h1>User Management system</h1>
+      //Routes
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signin" />} />
+          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
